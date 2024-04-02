a3f1d13a633077d9be26021f04d8ddbb24e880d5
Karthik73965 <karthikjidagam81@gmail.com>
2024-04-03 00:20:06 +0530

    navbar , hero section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Navbar from '@/components/Navbar'
+import HeroSection from '@/components/Home/HeroSection'
 type Props = {}
-
-export default function page({}: Props) {
+export default function page({ }: Props) {
   return (
-    <div>page</div>
+    <div className='body text-white'>
+      <Navbar />
+
+      <main className='  h-[200vh] pt-[100px]'>
+        <HeroSection/>
+      </main>
+    </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React , { useEffect, useState } from 'react'
+import { Button } from '@/ui/moving-border';
+import { RiArrowDropDownLine ,  } from 'react-icons/ri';
+type Props = {}
+
+export default function Navbar({}: Props) {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+  return (
+    <nav className={`fixed flex justify-between z-10   py-2 px-[10vw]  w-[100vw] ${isScrolled ? ' bg-black bg-opacity-65 border-b-[1px] border-gray-800 ' : 'bg-transparent '}`}>
+        <h1 className='text-xl font-sans pt-1'>Zyok</h1>
+        <ul className='flex gap-x-10 text-sm pt-2'>
+            <li className='flex'>Feautures <RiArrowDropDownLine size={20} className='text-gray-700'/></li>
+            <li>Docs</li>
+            <li>Plans</li>
+            <li>FAQ's</li>
+        </ul>
+        <Button
+       
+        className=" dark:bg-slate-900  w-40 h-10 text-white  border-slate-800"
+      >
+        Get Started
+      </Button>
+    </nav>
+  )
+}
