a9449291a00b08de4462b80dd4c8d97d10fb2be2
Karthik73965 <karthikjidagam81@gmail.com>
2024-04-14 03:16:06 +0530

    another fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider appearance={{
+    <ClerkProvider  publishableKey='pk_test_cGxlYXNhbnQtbWFsbGFyZC02OC5jbGVyay5hY2NvdW50cy5kZXYk' appearance={{
       baseTheme: dark,
       signIn: { baseTheme: dark },
       signUp:{baseTheme:dark}
